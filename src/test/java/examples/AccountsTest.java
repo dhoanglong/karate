0e2b3c72c15b2fcb96b0eaa73b89d5c4226718f8
long_dinh <long_dinh@epam.com>
2020-07-06 18:04:45 +0700

    added README

--- src/test/java/examples/AccountsTest.java
+++ src/test/java/examples/AccountsTest.java
@@ -6,20 +6,18 @@ import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AccountsTest {
     @Test
     public void testAccount() {
         Results results = Runner.path("classpath:examples/mec/").parallel(5);
         generateReport(results.getReportDir());
-        assertTrue(results.getErrorMessages(), results.getFailCount() == 0);
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
     }
 
     public static void generateReport(String karateOutputPath) {
--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -5,18 +5,11 @@ function fn() {
     env = 'dev'; // a custom 'intelligent' default
   }
   var port ='9080'
+
   var config = {
-//    baseURL: 'https://api.thecatapi.com/v1/',
-//    apiKey: '3e5c20e0-2875-4551-b983-d25c008d626c'
     baseURL: 'http://localhost:' + port + '/api/devices'
   };
-//  if (env == 'stage') {
-//    // over-ride only those that need to be
-//    config.baseURL = 'https://stage-host/v1/auth';
-//  } else if (env == 'e2e') {
-//    config.baseURL = 'https://e2e-host/v1/auth';
-//  }
-  // don't waste time waiting for a connection or if servers don't respond within 5 seconds
+
   karate.configure('connectTimeout', 5000);
   karate.configure('readTimeout', 5000);
   return config;
